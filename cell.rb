f70a61e65c0fcbee7ee39e8f97cf5fb2e5224138
Manz1999 <adri4n1999@gmail.com>
2023-06-07 19:52:06 -0600

    verification of status and  neighbours, modification of generations, new methods added
    Co-authored-by: limitedbug <firefing66@gmail.com>
    Co-authored-by: LuisCD2023 <ch.dl.luis2023@gmail.com>
    Co-authored-by: oscar-rios89 <oscar.rios0989@gmail.com>

--- cell.rb
+++ cell.rb
@@ -1,59 +1,50 @@
-class Cell {
-  def initialize(state = 0, position_rows, position_columns,wordl,x,y)
-    @world, @x, @y = world, x, y
-    @live = false
+# frozen_string_literal: true
+class Cell
+  def initialize(position_rows, position_columns, state = 0)
     @state = state
     @position_rows = position_rows
     @position_columns = position_columns
+    @neighbours = []
   end
 
-  def dead?
-    !@live
+  def new_state(newstate)
+    @state = newstate;
   end
 
-  def dead!
-    @live = false
+  def obtain_state
+    @state
+  end
+  #recibimos la copia
+  def verify_neighbours(board)
+
+    # (0,0) (0,1) (0,2)
+    # (1,0) (1,1) (1,2)
+    # (2,0) (2,1) (2,2)
+    (board[@position_rows - 1][@position_columns - 1]) # 0 0
+    (board[@position_rows - 1][@position_columns]) # 0 1
+    (board[@position_rows - 1][@position_columns + 1]) # 0 2
+    (board[@position_rows][@position_columns - 1]) # 1 0
+    (board[@position_rows][@position_columns + 1]) # 1 2
+    (board[@position_rows + 1][@position_columns - 1]) # 2 0
+    (board[@position_rows + 1][@position_columns]) # 2 1
+    (board[@position_rows + 1][@position_columns + 1]) # 2 2
   end
 
-  def live?
-    @live
+  #Cell Rules Test
+  def verify_state
+    live_neighbours = verify_live_neighbours
+    if(@state==1 && live_neighbours.length() < 2 || @state == 1 && live_neighbours.length() > 3) # kill cell
+      new_state(0)#add parameter
+    end
+    if(@state==0 && live_neighbours.length() == 3)# born a cell
+      new_state(1)
+    end
+
   end
-
-  def live!
-    @live = true
-  end
-
-  def toggle!
-    @live = !@live
-  end
-
- #def kill
-  #  @state = 0
-  #  @kill = false
-  #end
-
- #def live
- #  @state = 1
- #  @live = true
- #end
-
-  def neighbours
-    neighbours = []
-    neighbours.push(@world.cell_at(self.x - 1, self.y - 1))
-    neighbours.push(@world.cell_at(self.x - 1, self.y))
-    neighbours.push(@world.cell_at(self.x - 1, self.y + 1))
-    neighbours.push(@world.cell_at(self.x, self.y - 1))
-    neighbours.push(@world.cell_at(self.x, self.y + 1))
-    neighbours.push(@world.cell_at(self.x + 1, self.y - 1))
-    neighbours.push(@world.cell_at(self.x + 1, self.y))
-    neighbours.push(@world.cell_at(self.x + 1, self.y + 1))
-    neighbours
-  end
-
-  #We define a method to verify the lives of neighboring cells
-  def live_neighbours
-    self.neighbours.select do |n|
-        n && n.live
+  #We define a method to verifposition_columns the lives of neighboring cells
+  def verify_live_neighbours
+    @neighbours.select do |n|
+      n.obtain_state == 1
     end
   end
-}
+end
--- index.rb
+++ index.rb
@@ -1,52 +1,81 @@
-require 'cell'
+# frozen_string_literal: true
+require './cell'
 class GameOfLife
   def initialize(rows, columns)
     # we define our's variables
     @rows = rows
     @columns = columns
     @board = []
+    init_board
   end
 
   def play(generations = 1)
     i = 0;
     while i < generations
-      next_generation
+      @board = next_generation(@board)
+      print_board(@board)
+      i += 1
     end
   end
 
   def create_board
-    @board = Array.new(@rows) { Array.new(@columns, 0) } # we create our board with rows and columns
+    Array.new(@rows) { Array.new(@columns, 0) } # we create our board with rows and columns
   end
 
-  def fill_board
-    @board.each_with_index do |row, i| # we enter the board and get the element and his index
+  def fill_board(board)
+    board.each_with_index do |row, i| # we enter the board and get the element and his index
       row.each_with_index do |_, j| # same here with each row
-        @board[i][j] = Cell.new(rand(0..1), i, j)
+        board[i][j] = Cell.new(i, j, rand(0..1))
       end
     end
+    board
   end
 
-  def print_board
-    @board.each do |rows| # we print the rows with commas or spaces idk
-      p rows.join(' ')
+  def print_board(board)
+    board.each do |rows| # we print the rows with commas or spaces idk
+      msg_row = []
+      rows.each do |cell|
+        msg_row.push(cell.obtain_state)
+      end
+      p msg_row.join(' ')
     end
   end
 
-  def verify_neightbours()
-
+  def init_board
+    @board = fill_board(create_board)
+    print_board(@board)
   end
 
-  def next_generation()
-    @newboard = []
-    Cell.each do |cell|
-    if(cell.live && cell.live.live_neighbours < 2)
-      newboard.push cell
+  def next_generation(old_board)
+    newboard = create_board
+    copy_board = old_board.clone
+    old_board.each_with_index do |rows, i|
+      rows.each_with_index do |cell, j|
+        #por cada celula, le mandamos una copia y verificamos su estado
+        cell.verify_neighbours(copy_board)
+        newboard[i][j] = cell
+      end
     end
-    newboard.each(&:toggle!)
+    newboard
   end
 end
 
-game1 = GameOfLife.new(10, 10)
-game1.create_board
-game1.fill_board
-game1.print_board
+game1 = GameOfLife.new(3, 3)
+game1.play(1)
+
+
+# 1 2 3
+# 4 5 6
+# 7 8 9
+
+# 0 0 0       # 0 0 0       # 0 0 0
+# 0 1 2    1  # 1 2 3    2  # 2 3 0   3
+# 4 5 6       # 4 5 6       # 5 6 0
+
+# 0 1 2       # 1 2 3       # 2 3 0
+# 0 4 5    4  # 4 5 6   5   # 5 6 0   6
+# 0 7 8       # 7 8 9       # 8 9 0
+
+# 0 4 5       # 4 5 6       # 5 6 0
+# 0 7 8    7  # 7 8 9   8   # 8 9 0   9
+# 0 0 0       # 0 0 0       # 0 0 0
